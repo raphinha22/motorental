d9cdc1ae4bc40aa02aa713f3456f82f7140fbea9
Choup Phanha <nh4kh4444@gmail.com>
2024-04-04 07:52:15 +0700

    add more

--- src/Layouts/User.jsx
+++ src/Layouts/User.jsx
@@ -4,15 +4,18 @@ import HeadImgteam from "../Components/team/HeadImgteam";
 const User = () => {
   const data = useLoaderData();
   console.log(data);
-  const { login, avatar_url, url, location, created_at, updated_at } = data;
+  const { name, avatar_url, url, location, created_at, updated_at } = data;
   return (
     <div>
       <HeadImgteam />
       <div className="flex-detial">
         <img src={avatar_url} alt="image" />
         <div className="info">
-          <h2>Name: {login}</h2>
-          <p></p>
+          <h2>Name: {name}</h2>
+          <p>URL: {url}</p>
+          <p>Location: {location}</p>
+          <p>Create_At: {created_at}</p>
+          <p>Update_At: {updated_at}</p>
         </div>
       </div>
     </div>
